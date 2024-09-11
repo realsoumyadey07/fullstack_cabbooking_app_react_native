53f5781bb733d38228e8a8dc217e6d486f9ee719
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-09-11 19:29:39 +0530

    ok

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,13 @@
 import { View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <SafeAreaView className='flex-1 justify-center items-center bg-white'>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
 
