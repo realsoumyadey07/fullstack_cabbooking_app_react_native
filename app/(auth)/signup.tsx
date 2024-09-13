7c58af29f5fb5f625adfe9e8139706c39387b5db
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-09-13 08:17:39 +0530

    onBoarding screens are ready

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,11 +1,55 @@
-import { Text } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
+import InputField from "@/components/InputField";
+import { icons, images } from "@/constants";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native"
 
 const Signup = ()=> {
+     const [form, setForm] = useState({
+          name: "",
+          email: "",
+          password: "",
+     })
      return (
-          <SafeAreaView>
-               <Text>Signin</Text>
-          </SafeAreaView>
+          <ScrollView className="flex-1 bg-white">
+               <View className="flex-1 bg-white">
+                    <View className="relative w-full h-[250px]">
+                         <Image
+                         source={images.signUpCar}
+                         className="z-0 w-full h-[250px]"
+                         resizeMode="cover"
+                         />
+                         <Text className="text-2xl text-gray-600 font-JakartaSemiBold font-bold bottom-10 left-5">Create Your Account</Text>
+                    </View>
+                    <View className="p-5">
+                         <InputField
+                          label="Name"
+                          placeholder="Enter your name"
+                          icon={icons.person}
+                          labelStyle=""
+                          value={form.name}
+                          onChange={(value)=> setForm({...form, name: value})}
+                          />
+                         <InputField
+                          label="Name"
+                          placeholder="Enter your name"
+                          icon={icons.person}
+                          labelStyle=""
+                          value={form.name}
+                          onChange={(value)=> setForm({...form, name: value})}
+                          />
+                         <InputField
+                          label="Name"
+                          placeholder="Enter your name"
+                          icon={icons.person}
+                          labelStyle=""
+                          value={form.name}
+                          onChange={(value)=> setForm({...form, name: value})}
+                          />
+                    </View>
+               </View>
+               <StatusBar style="dark"/>
+          </ScrollView>
      )
 }
 
--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,14 +1,16 @@
+import CustomButton from "@/components/CustomButton";
 import { onboarding } from "@/constants";
 import { router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useRef, useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from "react-native-swiper";
 
 const OnBoarding = () => {
   const swiperRef = useRef<Swiper>(null);
-  const [activeItem, setActiveItem] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === onboarding.length - 1;
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
@@ -26,14 +28,26 @@ const OnBoarding = () => {
         activeDot={
           <View className="w-[32px] h-[4px] mx-1 bg-[#0286ff] rounded-full" />
         }
-        onIndexChanged={(index) => setActiveItem(index)}
+        onIndexChanged={(index) => setActiveIndex(index)}
       >
         {onboarding.map((item: any) => (
-          <View>
-               <Text>{item.title}</Text>
+          <View className="flex items-center justify-center">
+            <Image
+              source={item?.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10">{item?.title}</Text>
+            </View>
+            <Text className="text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">{item?.description}</Text>
           </View>
         ))}
       </Swiper>
+      <CustomButton
+        onPress={() => isLastSlide ? router.replace("/(auth)/signup") : swiperRef.current?.scrollBy(1)}
+        title={isLastSlide ? "Get Started" : "Next Slide"}
+        className="w-11/12 my-8 " />
       <StatusBar style="dark" />
     </SafeAreaView>
   );
