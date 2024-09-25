e30c1daec69576cea82eee996022d58f8b258968
Soumyadip Dey <soumyadipdey802@gmail.com>
2024-09-25 14:23:35 +0530

    map comp is being designed

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,22 +1,33 @@
+import { calculateRegion } from "@/lib/map";
+import { useLocationStore } from "@/store";
 import { Text, View } from "react-native";
-import MapView, { PROVIDER_DEFAULT } from "react-native-maps"
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
-export default function Map(){
-     const region = () => {
-
-     }
-     return (
-          <MapView
-          provider={PROVIDER_DEFAULT} 
-          className="w-full h-full"
-          tintColor="black"
-          mapType="mutedStandard"
-          showsPointsOfInterest={false}
-          // initialRegion={region}
-          showsUserLocation={true}
-          userInterfaceStyle="light"
-          >
-               <Text></Text>
-          </MapView>
-     )
+export default function Map() {
+  const {
+     userLatitude,
+     userLongitude,
+     destinationLatitude,
+     destinationLongitude
+  } = useLocationStore();
+  const region = calculateRegion({
+     userLatitude,
+     userLongitude,
+     destinationLatitude,
+     destinationLongitude
+  });
+  return (
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full"
+      tintColor="black"
+      mapType="mutedStandard"
+      showsPointsOfInterest={false}
+      // initialRegion={region}
+      showsUserLocation={true}
+      userInterfaceStyle="light"
+    >
+      <Text></Text>
+    </MapView>
+  );
 }
